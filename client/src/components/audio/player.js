a3b211593daa5fd06d9014ba17147da1c5dd45cd
FinMcB <43539940+FinMcB@users.noreply.github.com>
2020-02-16 16:49:38 +0000

    firstGridFS test

--- client/src/components/audio/player.js
+++ client/src/components/audio/player.js
@@ -12,15 +12,73 @@ class Player extends React.Component{
 		this.state = {
 			rotation: 0,
 			stateSketch: sketch,
+			files:[],
+			file:''
 		};
+		this.loadFiles = this.loadFiles.bind(this);
+
 	}
 
+	componentDidMount() {
+	this.loadFiles();
+}
+loadFiles() {
+	fetch('/api/files')
+		.then(res => res.json())
+		.then(files => {
+			if (files.message) {
+				console.log('No Files');
+				this.setState({ files: [] })
+			} else {
+				this.setState({ files })
+			}
+		});
+	}
+
+	fileChanged(event) {
+   const f = event.target.files[0];
+   this.setState({
+     file: f
+   });
+ }
+
+ deleteFile(event) {
+   event.preventDefault();
+   const id = event.target.id;
+
+   fetch('/api/files/'+id, {
+     method: 'DELETE'
+   }).then(res => res.json())
+     .then(response => {
+       console.log(response);
+       if (response.success) this.loadFiles()
+       else alert('Delete Failed');
+     })
+ }
+ uploadFile(event) {
+    event.preventDefault();
+    let data = new FormData();
+    data.append('file', this.state.file);
+   fetch('/api/files', {
+     method: 'POST',
+     body: data
+   }).then(res => res.json())
+     .then(data => {
+       if (data.success) {
+       this.loadFiles();
+     } else {
+       alert('Upload failed');
+     }
+   });
+ }
 
 
 
 
 
 	render () {
+		const { files } = this.state;
+
 		return (
 			<div className="col s1 center-align" style={{}}>
 
@@ -30,7 +88,31 @@ class Player extends React.Component{
 			</div>
 			<a id="micInitBtn" class="btn-floating btn-large waves-effect waves-light red"><i class="material-icons">add</i></a>
 			<a id="recBtn" class="btn-floating btn-large waves-effect waves-light red"><i class="material-icons">mic</i></a>
-
+				<input type="file" onChange={this.fileChanged.bind(this)}/>
+	          <button onClick={this.uploadFile.bind(this)}>Upload</button>
+	          <table className="App-table">
+	            <thead>
+	              <tr>
+	                  <th>File</th>
+	                  <th>Uploaded</th>
+	                  <th>Size</th>
+	                  <th></th>
+	              </tr>
+	            </thead>
+	            <tbody>
+	              {files.map((file, index) => {
+	                var d = new Date(file.uploadDate);
+	                return (
+	                  <tr key={index}>
+	                    <td><a href={`http://localhost:8000/api/files/${file.filename}`}>{file.filename}</a></td>
+	                    <td>{`${d.toLocaleDateString()} ${d.toLocaleTimeString()}`}</td>
+	                    <td>{(Math.round(file.length/100) / 10)+'KB'}</td>
+	                    <td><button onClick={this.deleteFile.bind(this)} id={file._id}>Remove</button></td>
+	                  </tr>
+	                )
+	              })}
+	            </tbody>
+	          </table>
 		</div>
 
 		// </div>
