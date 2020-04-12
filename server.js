8d55ef2ac5ad929dd76f54210e73fea90feb7889
FinMcB <43539940+FinMcB@users.noreply.github.com>
2020-04-12 18:00:14 +0100

    make it better

--- server.js
+++ server.js
@@ -61,7 +61,16 @@ app.post('/api/users', upload.single('soundBlob'), function (req, res, next) {
 // app.use(express.static('/api/users'))
 
 
+//Serve static assets if in production
 
+if(process.env.NODE_ENV === 'production') {
+  //Set static folder
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+
+}
 
 const port = process.env.PORT || 8000;
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
